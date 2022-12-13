dc1ee57f6df63b8295dd33d70a1774662c05a3b7
Pankaj Pandey <pankaj.p@vcloudx.com>
2022-12-13 19:59:35 +0530

    update latest sdk

--- app/src/main/java/com/enablex/demoenablex/activity/VideoConferenceActivity.java
+++ app/src/main/java/com/enablex/demoenablex/activity/VideoConferenceActivity.java
@@ -623,20 +623,6 @@ public class VideoConferenceActivity extends AppCompatActivity
 
     }
 
-    @Override
-    public void onACKStartLiveTranscription(JSONObject jsonObject) {
-
-    }
-
-    @Override
-    public void onACKStopLiveTranscription(JSONObject jsonObject) {
-
-    }
-
-    @Override
-    public void onTranscriptionEvents(JSONObject jsonObject) {
-
-    }
 
     @Override
     public void onActiveTalkerView(RecyclerView recyclerView) {
