cdd5448ad9e30eed703c9fffe2179e6379606ad2
Enablex <pankaj.p@vcloudx.com>
2023-12-14 14:55:14 +0530

    update uikit sdk and packages

--- app/src/main/java/com/enablex/demoenablex/web_communication/WebConstants.java
+++ app/src/main/java/com/enablex/demoenablex/web_communication/WebConstants.java
@@ -12,8 +12,8 @@ public class WebConstants {
     /*The following information required, Only when kTry = true, When you hosted your own webservice remove these fileds*/
 
     /*Use enableX portal to create your app and get these following credentials*/
-    public static final String kAppId = "App_ID";
-    public static final String kAppkey = "App_Key";
+    public static final String kAppId = "App-ID";
+    public static final String kAppkey = "App-Key";
 
 
     public static final String getRoomId = "createRoom/";
